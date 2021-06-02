66e7b0e0261a5508a12c8390e71551d372fe87e4
Bognar <bognarvojislav791@gmail.com>
2021-06-01 21:32:57 -0700

    ...

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{any}', 'SpaController@index')->where('any', '.*');
-Route::post('/paymentResult', 'API\ClientController@paymentResult');
+Route::post('/paymentResult', 'Api\ClientController@paymentResult');
 
 
